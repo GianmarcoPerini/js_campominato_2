04916aba269757386b55812ff2e248f7516efbf6
GianmarcoPerini <g.marco.perini@gmail.com>
2021-09-11 18:44:53 +0200

    campoFinito01

--- main.js
+++ main.js
@@ -4,40 +4,44 @@ const fieldGeneretor = () => {
     const bomb = Array(20).fill('explode')
     const arrConcat = valid.concat(bomb)
     const sorted = arrConcat.sort(() => Math.random() - 0.5)
-
     for(let i=0;i<100;i++){
         const square = document.createElement('div');
         square.setAttribute('id', i)
         square.classList.add(sorted[i])
         grid.appendChild(square)
     }
-};
-
+}
+fieldGeneretor()
 const checkBomb = () => {
     const check = document.querySelectorAll('.grid div');
-    let count = 0
-    for(let i=0;i<100;i++){
-        const currentCheck = check[i]
-        currentCheck.addEventListener('click', function(){
-            if(this.classList == 'explode'){
-                currentCheck.classList.add('bomb')
-                setTimeout(()=>{
-                    window.location.reload(true)
-                },500)
-            }else{
-                currentCheck.classList.add('green')
-                count++
-                document.getElementById('result').innerHTML = count
-                if(count == 10){
-                document.getElementById('result').innerHTML = 'VITTORIA'
-                setTimeout(()=>{
-                    window.location.reload(true)
-                },500)
+    let count = 00
+        for(let i=0;i<100;i++){
+            const currentCheck = check[i]
+            currentCheck.addEventListener('click', function(){
+                if(this.classList == 'explode'){
+                    currentCheck.classList.add('bomb')
+                    document.querySelector('#face').innerHTML = `<i class="fs-1 text-warning far fa-frown"></i>`
+                    setTimeout(()=>{
+                        window.location.reload(true)
+                    },500)
+                }else{
+                    currentCheck.classList.add('green')
+                    count++
+                    document.getElementById('c1').innerHTML = round(count)
+                    if(count == 10){
+                        document.querySelector('#face').innerHTML = `<i class="fs-1 text-warning far fa-star"></i>`
+                        setTimeout(()=>{
+                            window.location.reload(true)
+                        },500)
+                    }
                 }
-            }
-        })
+            })
+        }
     }
+    checkBomb()
+
+    
+function round(n){
+    return n > 9 ? "" + n: "0" + n;
 }
 
-fieldGeneretor()
-checkBomb()
